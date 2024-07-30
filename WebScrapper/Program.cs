49c3fbb49940f656921c6dc4cb564dec441a6bf8
Anuththara Perera <anuththaraperera14@gmail.com>
2024-07-30 14:17:20 +0530

    Integrated the Get request to Weather.com

--- WebScrapper/Program.cs
+++ WebScrapper/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "https://weather.com/weather/today/l/220847d71fafc35aa68b0a22ac9bd012d25dcf161e70c8d18ac014f973b95563";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+        }
+    }
+}
